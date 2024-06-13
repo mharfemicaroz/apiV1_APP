8ca9ea98d4398373482b778b2fe89db5f8e9a658
unknown <mharfemicaroz@gmail.com>
2024-06-14 06:40:37 +0800

    proxy

--- vue-vite-bootstrap/vite.config.js
+++ vue-vite-bootstrap/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://apiv1-phi.vercel.app",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
